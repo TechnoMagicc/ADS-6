e85f0d47c5a0799c6c38e670a4150b966a204323
TechnoMagicc <leyzarof@gmail.com>
2025-05-04 22:47:14 +0300

    Add files via upload

--- include/tpqueue.h
+++ include/tpqueue.h
@@ -1,53 +1,54 @@
 // Copyright 2022 NNTU-CS
 #ifndef INCLUDE_TPQUEUE_H_
 #define INCLUDE_TPQUEUE_H_
-#include <stdexcept>
+#include <string>
+
 template<typename T>
 class TPQueue {
-    private:
-        struct Node {
-            T data;
-            Node* next;
-        };
-    
-        Node* head_;
-    
-    public:
-        TPQueue() : head_(nullptr) {}
-        ~TPQueue() {
-            while (head_ != nullptr) {
-                Node* temp = head_;
-                head_ = head_->next;
-                delete temp;
-            }
+ private:
+    struct Node {
+        T data;
+        Node* next;
+        explicit Node(const T& data, Node* next = nullptr)
+            : data(data), next(next) {}
+    };
+    Node* head;
+ public:
+    TPQueue() : head(nullptr) {}
+    ~TPQueue() {
+        while (head) {
+            Node* temp = head;
+            head = head->next;
+            delete temp;
         }
-        void enqueue(const T& item) {
-            Node* new_node = new Node{item, nullptr};
-            Node* prev = nullptr;
-            Node* current = head_;
-            while (current != nullptr && current->data.prior >= item.prior) {
-                prev = current;
-                current = current->next;
-            }
-            new_node->next = current;
-            if (prev == nullptr) {
-                head_ = new_node;
-            } else {
-                prev->next = new_node;
-            }
+    }
+    void push(const T& item) {
+        if (!head || item.prior > head->data.prior) {
+            head = new Node(item, head);
+            return;
         }
-        T dequeue() {
-            if (head_ == nullptr) {
-                throw std::runtime_error("Dequeue on empty queue");
-            }
-    
-            Node* to_remove = head_;
-            T result = to_remove->data;
-            head_ = head_->next;
-    
-            delete to_remove;
-            return result;
+        Node* cur = head;
+        while (cur->next &&
+               cur->next->data.prior >= item.prior) {
+            cur = cur->next;
         }
+        cur->next = new Node(item, cur->next);
+    }
+    T pop() {
+        if (!head) {
+            throw std::string("Empty");
+        }
+        T res = head->data;
+        Node* temp = head;
+        head = head->next;
+        delete temp;
+        return res;
+    }
+    bool isEmpty() const {
+        return head == nullptr;
+    }
+    TPQueue(const TPQueue&) = delete;
+    TPQueue& operator=(const TPQueue&) = delete;
 };
 struct SYM {
   char ch;
